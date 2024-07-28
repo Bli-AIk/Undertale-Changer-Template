9908ed8dce963a4a2ce00604b634834b8bd16651
Bli-AIk <haikun2333@126.com>
2024-07-28 20:48:14 +0800

    TurnEditor start

---
+++ Assets/Editor/TurnEditor.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class TurnEditor : EditorWindow
+{
+    [MenuItem("UCT Window/TurnEditor")]
+    public static void ShowExample()
+    {
+        TurnEditor wnd = GetWindow<TurnEditor>();
+        wnd.titleContent = new GUIContent("TurnEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/TurnEditor.uxml");
+        //VisualElement labelFromUXML = visualTree.Instantiate();
+        //root.Add(labelFromUXML);
+        visualTree.CloneTree(root);
+
+    }
+}
