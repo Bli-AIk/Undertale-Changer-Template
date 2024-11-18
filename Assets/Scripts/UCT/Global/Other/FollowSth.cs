5a5d905e28190298f1b86ad9e1ca1f7c457d4654
AIk <111052929+Bli-AIk@users.noreply.github.com>
2024-11-18 18:29:51 +0800

    v1.0.3-part11f
    修复OW的后处理
    
    Post processing for repairing OW

--- Assets/Scripts/UCT/Global/Other/FollowSth.cs
+++ Assets/Scripts/UCT/Global/Other/FollowSth.cs
@@ -1,18 +1,33 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace UCT.Global.Other
 {
     public class FollowSth : MonoBehaviour
     {
+        public string sthName;
         public GameObject sth;
-        public Vector3 posAdd;
+        public bool followPosition;
+        public Vector3 positionAdd;
+        public bool followRotation;
+        public Vector3 rotationAdd;
+        public bool followLocalScale;
+        public Vector3 localScaleAdd;
+
+        private void Start()
+        {
+            if (!sth && !string.IsNullOrEmpty(sthName))
+                sth = GameObject.Find(sthName);
+        }
 
         private void Update()
         {
-            if(sth != null)
-            {
-                transform.position = sth.transform.position + posAdd;
-            }
+            if (!sth) return;
+            if (followPosition) transform.position = sth.transform.position + positionAdd;
+            if (followRotation) transform.rotation = sth.transform.rotation * Quaternion.Euler(rotationAdd);
+            if (followLocalScale) transform.localScale = sth.transform.localScale + localScaleAdd;
+
         }
     }
 }
